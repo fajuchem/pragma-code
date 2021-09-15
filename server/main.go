03d841d45b126914c36e7bf26678b00b7a304f95
“Maksim <maksim.tsvetkov@service.nsw.gov.au>
2021-09-15 12:07:49 +0300

    feat: expose client app via go server

--- server/main.go
+++ server/main.go
@@ -5,11 +5,31 @@ import (
 	"log"
 	"net/http"
 	"server/router"
+	"time"
 )
+
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+func (s spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.FileServer(http.Dir("../client/build")).ServeHTTP(w, r)
+}
 
 func main() {
 	r := router.Router()
 	fmt.Println("Starting server on the port 4000...")
 
-	log.Fatal(http.ListenAndServe(":4000", r))
+	spa := spaHandler{staticPath: "../client/build", indexPath: "index.html"}
+	r.PathPrefix("/").Handler(spa)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:4000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -2,10 +2,11 @@ package middleware
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"server/models"
 )
 
@@ -73,9 +74,7 @@ func GetAllProducts(w http.ResponseWriter, _ *http.Request) {
 
 	resultChannel := make(chan models.ResultsItem)
 
-	for i, beer := range beers {
-		fmt.Println(i, beer.Name)
-
+	for _, beer := range beers {
 		go SendGetSensorAsync(beer.Id, resultChannel)
 	}
 
@@ -88,9 +87,6 @@ func GetAllProducts(w http.ResponseWriter, _ *http.Request) {
 
 	for j, beer := range beers {
 		beers[j].Temperature = result[beer.Id]
-
-		fmt.Println(result[beer.Id])
-		fmt.Println(beer)
 
 		if result[beer.Id] < beer.MinimumTemperature {
 			beers[j].TemperatureStatus = "too low"
@@ -127,4 +123,43 @@ func SendGetSensorAsync(id string, rc chan models.ResultsItem) {
 		panic(err)
 	}
 	rc <- models.ResultsItem{Id: id, Res: sensor}
+}
+
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
